ed19ec1e0f67130d5941ef4bb796c9f19cc1c95b
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2024-04-08 20:29:59 -0500

    Testing js function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,22 @@
-console.log('Hello, world 1!');
-console.log('Hello, world 2!');
+const $=selector => document.querySelector(selector);
+const $$=selector => document.querySelectorAll(selector);
 
+// get by id name, cel , email and message
+const name = $('#name');
+const cel = $('#phone');
+const email = $('#email');
+const message = $('#message');
 
+const form = $('form');
 
-const a=3;
-console.log(`Hello, world 3${a}!`);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = {
+        name: name.value,
+        cel: cel.value,
+        email: email.value,
+        message: message.value
+    };
+    console.log(data);
+    form.reset();
+});
